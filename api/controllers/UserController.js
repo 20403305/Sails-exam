47d7b5d38339eefae0de68c379e0f88322273232
Su Yuxin <20403305@life.hkbu.edu.hk>
2020-12-28 14:54:04 +0800

    enquiry--正则表达式+比较格式和位大小

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,17 +9,41 @@ module.exports = {
       // action - read
       enquiry: async function (req, res) {
 
-        var thatPerson = await Person.findOne({ markid: 1 });
+        if (req.method == "GET") return res.view('user/enquiry');
 
-        if (!thatPerson) return res.notFound();
+        if (req.wantsJSON) {
+            var eat = await User.create(req.body).fetch();
 
-        return res.view('person/enquiry', { person: thatPerson });
+
+            if ( req.body.preferstart > "17:00") return res.json("The time > 17:00, you will be contacted the day after tomorrow");
+            if ( req.body.preferstart < "17:00") return res.json("The time < 17:00, you will be contacted tomorrow");
+            return res.json("Create Success");
+        } else {
+            return res.redirect('/');			// for normal request
+        }
+
+        // if (!req.body.username || !req.body.password) return res.badRequest();
+
+        // var user = await User.findOne({ username: req.body.username });
+
+        // if (!user) return res.status(401).json("User not found");
+
+        // var match = await sails.bcrypt.compare(req.body.password, user.password);
+        // if (!match) return res.status(401).json("Wrong Password");
+
+
+
+        // var thatPerson = await User.findOne({ emailAddress: "123456@like.com" });
+
+        // if (!thatPerson) return res.notFound();
+
+        // return res.view('user/enquiry', { person: thatPerson });
     },
 
     // json function
     enquiries: async function (req, res) {
 
-        var everyones = await Person.find();
+        var everyones = await User.find();
 
         return res.json(everyones);
     },
